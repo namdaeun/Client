00b4e769b8db7e7fc4f1617d3e0c41eaa941d37a
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-06-10 00:44:29 +0900

    :recycle: refactor: 폴더구조 변경

--- note-app/src/components/BoxHeader/BoxHeader.tsx
+++ note-app/src/components/BoxHeader/BoxHeader.tsx
@@ -1,7 +1,7 @@
 import { css } from "@emotion/react";
 import { circleStyle, dotWrapper, wrapper } from "./BoxHeader.style";
-import { Theme } from '../../../styles/theme';
-import { useTheme } from "../../../context/theme";
+import { useTheme } from "../../context/theme";
+import { Theme } from "../../styles/theme";
 
 export const BoxHeader = () => {
   const { toggleTheme } = useTheme();
