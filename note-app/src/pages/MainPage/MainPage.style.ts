2a767863255333db8bc6e9711b189338a504b8b9
namdaeun <nde40345@gmail.com>
2024-06-10 08:18:33 +0900

    :sparkles: feat: 페이지네이션 메인 페이지에 불러오기

--- note-app/src/pages/MainPage/MainPage.style.ts
+++ note-app/src/pages/MainPage/MainPage.style.ts
@@ -7,3 +7,22 @@ export const barWrapper = css`
 
   padding-bottom: 0.7rem;
 `;
+
+export const bottomwrapper = css`
+  display: flex;
+  align-items: center;
+`;
+
+export const paginationWrapper = css`
+  position: absolute;
+  top: 73%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+export const buttonWrapper = css`
+  position: absolute;
+  top: 71.5%;
+  right: 30%;
+  margin-left: auto;
+`;
--- note-app/src/pages/MainPage/MainPage.tsx
+++ note-app/src/pages/MainPage/MainPage.tsx
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from '../../components/Button/Button';
 import DropDown from '../../components/DropDown/DropDown';
 import Title from '../../components/Title/Title';
-import { barWrapper } from './MainPage.style';
+import { barWrapper, bottomwrapper, buttonWrapper, paginationWrapper } from './MainPage.style';
 import MemoList from './MemoList/MemoList';
+import Pagination from './Pagination/Pagination';
 import Quote from './Quote/Quote';
 import SearchBar from './SearchBar/SearchBar';
 
@@ -12,19 +14,50 @@ const MainPage = () => {
 
   const handleBtnClick = () => {
     navigate('/add');
-  }
+  };
+
+  const dummyMemoData = [
+    { id: 1, title: 'Memo 1' },
+    { id: 2, title: 'Memo 2' },
+    { id: 3, title: 'Memo 3' },
+    { id: 4, title: 'Memo 4' },
+    { id: 5, title: 'Memo 5' },
+    { id: 6, title: 'Memo 6' },
+    { id: 7, title: 'Memo 7' },
+    { id: 8, title: 'Memo 8' },
+    { id: 9, title: 'Memo 9' },
+  ];
+
+  const [page, setPage] = useState(1);
+  const limit = 3;
+
+  const totalMemo = dummyMemoData.length;
+
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const currentMemoData = dummyMemoData.slice(startIndex, endIndex);
+
   return (
     <>
       <Title>Jinda Note</Title>
       <Quote />
       <section css={barWrapper}>
-        <SearchBar/>
+        <SearchBar />
         <DropDown />
       </section>
-      <MemoList date='몇 초' >리액트 토이 프로젝트</MemoList>
-      <MemoList date='몇 초' >리액트 토이 프로젝트</MemoList>
-      <MemoList date='몇 초' >리액트 토이 프로젝트</MemoList>
-      <Button handleBtnClick={handleBtnClick}>새 노트</Button>
+      {currentMemoData.map((memo) => (
+        <MemoList key={memo.id} date="몇 초">
+          {memo.title}
+        </MemoList>
+      ))}
+      <section css={bottomwrapper}>
+        <div css={paginationWrapper}>
+          <Pagination totalMemo={totalMemo} limit={limit} page={page} setPage={setPage} />
+        </div>
+        <div css={buttonWrapper}>
+          <Button handleBtnClick={handleBtnClick}>새 노트</Button>
+        </div>
+      </section>
     </>
   );
 };
