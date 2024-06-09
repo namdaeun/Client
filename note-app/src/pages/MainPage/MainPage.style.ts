5d57a50798ca1667be804f0ddb25aae6fe3109cb
namdaeun <nde40345@gmail.com>
2024-06-10 06:04:51 +0900

    :lipstick: style: 검색 바와 드롭다운 사이 gap 조정

--- note-app/src/pages/MainPage/MainPage.style.ts
+++ note-app/src/pages/MainPage/MainPage.style.ts
@@ -1,7 +1,8 @@
-import { css } from "@emotion/react";
+import { css } from '@emotion/react';
 
 export const barWrapper = css`
   display: flex;
+  gap: 0.5rem;
   justify-content: space-between;
 
   padding-bottom: 0.7rem;
