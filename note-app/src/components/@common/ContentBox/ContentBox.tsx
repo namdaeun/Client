85edffd11aaa6e58f0f4ddca8c016bb7ca2bbc7c
namdaeun <nde40345@gmail.com>
2024-06-10 02:13:15 +0900

    :bug: fix: ContentBox에 isFocus prop 추가

--- note-app/src/components/@common/ContentBox/ContentBox.tsx
+++ note-app/src/components/@common/ContentBox/ContentBox.tsx
@@ -1,32 +1,34 @@
 import React from 'react';
-import { BoxStyleProps, getBoxStyle } from './ContentBox.style';
 import { useTheme } from '../../../context/theme';
 import { hexToRGBA } from '../../../utils/hexToRGBA';
+import { BoxStyleProps, getBoxStyle } from './ContentBox.style';
 
 export interface BoxProps extends React.HTMLAttributes<HTMLDivElement> {
   styles?: BoxStyleProps;
   variant?: 'default' | 'content';
+  isFocus?: boolean;
 }
 
-const ContentBox = ({ children, styles = {}, variant = 'default' }: BoxProps) => {
-  const {theme} = useTheme();
+const ContentBox = ({ children, styles = {}, variant = 'default', isFocus }: BoxProps) => {
+  console.log(isFocus);
+  const { theme } = useTheme();
   const customStyle = {
-    'default': {
+    default: {
       width: '50vh',
       height: '60vh',
-      
+
       padding: '1rem',
       borderRadius: '2rem',
-      backgroundColor: hexToRGBA({hex: theme.bgColor, opacity: 0.589}),
+      backgroundColor: hexToRGBA({ hex: theme.bgColor, opacity: 0.589 }),
       color: theme.textColor,
     },
-    'content': {
-      padding: '1rem',
+    content: {
       borderRadius: '0.5rem',
       backgroundColor: theme.bgColor,
       color: theme.textColor,
+      border: isFocus ? `1.5px solid ${theme.point2Color}` : `1.5px solid transparent`,
     },
-  }
+  };
 
   return <div css={getBoxStyle({ ...styles, ...customStyle[variant] })}>{children}</div>;
 };
