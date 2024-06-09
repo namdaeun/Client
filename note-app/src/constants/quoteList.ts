44332213b4004f93660b4b201417a0645956c4ff
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-06-10 00:44:50 +0900

    :sparkles: feat: quoteList 상수 추가

---
+++ note-app/src/constants/quoteList.ts
@@ -0,0 +1,12 @@
+export const quoteList = [
+    'Memories are timeless treasures of the heart.',
+    'We don’t remember days, we remember moments.',
+    'People change. Memories don’t.',
+    'Write it down, before it’s forgotten.',
+    'Memories fade, but words are eternal.',
+    'Preserve today, relive tomorrow.',
+    'From thoughts to words, from memories to legacy.',
+    'Keep a note, hold the thought.',
+    'Ink your thoughts, cherish your life.',
+    'Every note, a step to your past.'
+]
