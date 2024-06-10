6117839a06aff54d1e334286cd76b33558ad09b9
namdaeun <nde40345@gmail.com>
2024-06-10 12:06:10 +0900

    :bug: fix: 이미지 import로 불러옴

--- note-app/src/context/theme.tsx
+++ note-app/src/context/theme.tsx
@@ -1,27 +1,29 @@
-import { ReactNode, createContext, useContext, useState } from "react";
+import { ReactNode, createContext, useContext, useState } from 'react';
+import darkBackground from 'src/assets/darkBackground.png';
+import lightBackground from 'src/assets/lightBackground.png';
 
 export type themeProps = {
-  bgColor: string,
-  textColor: string,
+  bgColor: string;
+  textColor: string;
 
-  point1Color: string,
-  point2Color: string,
+  point1Color: string;
+  point2Color: string;
 
-  redBorderColor: string,
-  backgroundImg: string,
+  redBorderColor: string;
+  backgroundImg: string;
 
-  switchColor: string,
+  switchColor: string;
 };
 
 const lightTheme = {
   bgColor: '#FFFFFF',
   textColor: '#000000',
-  
+
   point1Color: '#AAABAA',
   point2Color: '#E5BEA7',
 
   redBorderColor: '#FC5230',
-  backgroundImg: 'src/assets/lightBackground.png',
+  backgroundImg: lightBackground,
 
   switchColor: '#595959',
 };
@@ -34,27 +36,23 @@ const darkTheme = {
   point2Color: '#446DA3',
 
   redBorderColor: '#AE3A1E',
-  backgroundImg: 'src/assets/darkBackground.png',
+  backgroundImg: darkBackground,
   switchColor: 'lightgray',
 };
 
 const ThemeContext = createContext({
   theme: lightTheme,
-  toggleTheme: () => {}
+  toggleTheme: () => {},
 });
 
-export const ThemeModeProvider = ({children}: {children: ReactNode}) => {
+export const ThemeModeProvider = ({ children }: { children: ReactNode }) => {
   const [theme, setTheme] = useState<themeProps>(lightTheme);
 
   const toggleTheme = () => {
-    setTheme(prev => prev === lightTheme ? darkTheme : lightTheme);
-  }
+    setTheme((prev) => (prev === lightTheme ? darkTheme : lightTheme));
+  };
 
-  return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      {children}
-    </ThemeContext.Provider>
-  )
-}
+  return <ThemeContext.Provider value={{ theme, toggleTheme }}>{children}</ThemeContext.Provider>;
+};
 
 export const useTheme = () => useContext(ThemeContext);
