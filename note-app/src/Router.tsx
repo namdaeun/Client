ae158a9f93466767420fd69507df06e3bc7b1505
namdaeun <nde40345@gmail.com>
2024-06-10 11:49:38 +0900

    :art: chore: 사용 안 하는 import문 제거

--- note-app/src/Router.tsx
+++ note-app/src/Router.tsx
@@ -1,17 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import WritingPage from './pages/WritingPage/WritingPage';
-import EditPage from './pages/EditPage/EditPage';
 import MainPage from './pages/MainPage/MainPage';
 import ViewPage from './pages/ViewPage/ViewPage';
+import WritingPage from './pages/WritingPage/WritingPage';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
-        <Route path="/edit" element={<WritingPage variant='edit'/>} />
+        <Route path="/edit" element={<WritingPage variant="edit" />} />
         <Route path="/view" element={<ViewPage />} />
-        <Route path="/add" element={<WritingPage variant='add'/>} />
+        <Route path="/add" element={<WritingPage variant="add" />} />
       </Routes>
     </BrowserRouter>
   );
