728ee6d6034c2c16934d4c2f629cad8ab04a0ca3
namdaeun <nde40345@gmail.com>
2024-06-10 12:22:43 +0900

    :bug: fix: alert문구 수정

--- note-app/src/utils/copyLink.ts
+++ note-app/src/utils/copyLink.ts
@@ -1,9 +1,8 @@
 export const copyLink = async (link: string) => {
   try {
     await navigator.clipboard.writeText(link);
-    alert('초대 링크가 복사되었습니다.');
+    alert('링크가 복사되었습니다.');
   } catch (err) {
     console.error('클립보드 복사 실패:', err);
-    alert('클립보드 복사 실패');
   }
 };
