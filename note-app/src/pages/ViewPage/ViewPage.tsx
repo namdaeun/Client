9ced232c90c0c9b1b4b7d9a7d57184c8e7c2f474
namdaeun <nde40345@gmail.com>
2024-06-10 12:19:49 +0900

    :sparkles: feat: 공유하기 함수 전달

--- note-app/src/pages/ViewPage/ViewPage.tsx
+++ note-app/src/pages/ViewPage/ViewPage.tsx
@@ -8,6 +8,7 @@ import Title from '../../components/Title/Title';
 import { useTheme } from '../../context/theme';
 import { NoteProps, useFetchData } from '../../hooks/useFetchData';
 import { Theme } from '../../styles/theme';
+import { copyLink } from '../../utils/copyLink';
 import * as s from './ViewPage.style';
 
 const ViewPage = () => {
@@ -29,18 +30,24 @@ const ViewPage = () => {
   const handleEditClick = () => navigate('/edit', { state: noteId });
 
   const handleBackClick = () => navigate('/');
-  const handleShareClick = () => {};
+  const handleShareClick = () => {
+    copyLink(`https://note-app-omega-one.vercel.app/view`);
+  };
+
   return (
     <main css={s.wrapper}>
       <Title>{data?.title}</Title>
-      <ContentBox variant="content" styles={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'start',
-        justifyContent: 'space-between',
-        height: '80%',
-        padding: '0.5rem'
-      }}>
+      <ContentBox
+        variant="content"
+        styles={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'start',
+          justifyContent: 'space-between',
+          height: '80%',
+          padding: '0.5rem',
+        }}
+      >
         <article css={s.content}>{data?.content}</article>
         <section css={s.iconWrapper}>
           <FontAwesomeIcon
@@ -57,7 +64,7 @@ const ViewPage = () => {
         <Button variant="secondary" handleBtnClick={handleBackClick}>
           뒤로가기
         </Button>
-        <Button variant="default" handleBtnClick={handleShareClick}>
+        <Button variant="default" handleBtnClick={handleShareClick} onClick={handleShareClick}>
           공유하기
         </Button>
       </section>
